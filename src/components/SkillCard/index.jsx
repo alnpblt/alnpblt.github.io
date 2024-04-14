f8d31a0fe2abb9d3503f5935c4e864a6c6f8e266
Allen Poblete <pobleteallen.developer@gmail.com>
2024-04-14 15:43:43 +0800

    update config

--- src/components/SkillCard/index.jsx
+++ src/components/SkillCard/index.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 export default function SkillCard({description, icon, className}){
   return (
     <div className={`w-32 p-6 bg-secondary rounded-lg shadow text-light text-center flex flex-col ${className}`}>
-      <Image className='w-14 self-center drop-shadow' src={`/static/images/${icon}`} alt='icon' width={0} height={0}/>
+      <Image className='w-14 self-center drop-shadow' src={`${process.env.NEXT_PUBLIC_BASE_PATH}/static/images/${icon}`} alt='icon' width={0} height={0}/>
       <p className='text-xs mt-2 font-semibold'>{description}</p>
     </div>
   );
