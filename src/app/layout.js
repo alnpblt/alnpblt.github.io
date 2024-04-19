3a0d15bae7aeb70352b68cadc1a6a9c74cee4d79
Allen Poblete <pobleteallen.developer@gmail.com>
2024-04-19 09:57:49 +0800

    added metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,11 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Allen Poblete Portfolio",
+  openGraph: {
+    title: "Allen Poblete Portfolio",
+    description: "Experienced backend developer specializing in RESTful APIs, Javascript(Node.js), PHP, MySQL and Docker. Passionate about crafting efficient and scalable solutions. Let‘s turn your ideas into reality!",
+    siteName: 'Allen Poblete Portfolio'
+  }
 };
 
 export default function RootLayout({ children }) {
